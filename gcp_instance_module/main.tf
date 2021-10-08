f3ae70561e7da9fe725f83a6f4d024804696dee6
Sunveer Seera <sunveer.seera@pcmsgroup.com>
2021-10-08 11:14:17 +0100

    testing ubutnu image

--- gcp_instance_module/main.tf
+++ gcp_instance_module/main.tf
@@ -8,7 +8,7 @@ resource "google_compute_instance" "default" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      mage = "ubuntu-os-cloud/ubuntu-1604-lts"
     }
   }
 
